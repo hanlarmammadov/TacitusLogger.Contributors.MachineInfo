3b917f70b071c3edaeace9c4ae5a923d0069ef19
Khanlar Mammadov <khanlar@mammadov.pro>
2020-02-28 20:21:59 +0400

    Adding configuration examples

---
+++ examples/TacitusLogger.Contributors.MachineInfo.Examples/Configuring.cs
@@ -0,0 +1,31 @@
+﻿
+namespace TacitusLogger.Contributors.MachineInfo.Examples
+{
+    class Configuring
+    {
+        public void Adding_Machine_Info_Contributor_To_The_Logger()
+        {
+            MachineInfoContributor machineInfo = new MachineInfoContributor(); 
+
+            Logger logger = new Logger();
+            logger.AddLogContributor(machineInfo); 
+        }
+        public void Explicitly_Specifying_Name_And_Status_Of_Machine_Info_Contributor()
+        {
+            MachineInfoContributor machineInfo = new MachineInfoContributor("Source machine");
+            machineInfo.SetActive(true);
+
+            Logger logger = new Logger();
+            logger.AddLogContributor(machineInfo);
+        }
+        public void Explicitly_Specifying_Mutable_Status_Of_Machine_Info_Contributor()
+        {
+            MutableSetting<bool> status = Setting<bool>.From.Variable(true); 
+            MachineInfoContributor machineInfo = new MachineInfoContributor("Source machine");
+            machineInfo.SetActive(status);
+
+            Logger logger = new Logger();
+            logger.AddLogContributor(machineInfo); 
+        }
+    }
+}
---
+++ examples/TacitusLogger.Contributors.MachineInfo.Examples/ConfiguringWithBuilders.cs
@@ -0,0 +1,40 @@
+﻿using TacitusLogger.Builders;
+
+namespace TacitusLogger.Contributors.MachineInfo.Examples
+{
+    class ConfiguringWithBuilders
+    {
+        public void Adding_Machine_Info_Contributor_To_The_Logger()
+        {
+            Logger logger = LoggerBuilder.Logger()
+                                         .Contributors()
+                                             .MachineInfo()
+                                         .BuildContributors()
+                                         .ForAllLogs()
+                                             .Console().Add()
+                                         .BuildLogger();
+        }
+        public void Explicitly_Specifying_Name_And_Status_Of_Machine_Info_Contributor()
+        {
+            Logger logger = LoggerBuilder.Logger()
+                                         .Contributors()
+                                             .MachineInfo(true, "Source machine")
+                                         .BuildContributors()
+                                         .ForAllLogs()
+                                             .Console().Add()
+                                         .BuildLogger();
+        }
+        //public void Explicitly_Specifying_Mutable_Status_Of_Machine_Info_Contributor()
+        //{
+        //    MutableSetting<bool> status = Setting<bool>.From.Variable(true);
+
+        //    Logger logger = LoggerBuilder.Logger()
+        //                                 .Contributors()
+        //                                     .MachineInfo(status)
+        //                                 .BuildContributors()
+        //                                 .ForAllLogs()
+        //                                     .Console().Add()
+        //                                 .BuildLogger();
+        //}
+    }
+}
